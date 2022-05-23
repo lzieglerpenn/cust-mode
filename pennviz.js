4e344c704a1b0c7112e8e911b016eddb413bd251
lzieglerpenn <lee.ziegler@penn-interactive.com>
2022-05-23 12:03:20 -0400

    first commit to main

--- pennviz.js
+++ pennviz.js
@@ -6,6 +6,15 @@ var pennviz = {
     var df = new dfd.DataFrame(holder);
     return df
   }
+
+  mergeQuerys: function(left_query, right_query, on, how='inner'){
+      var left = loadQueryData(left_query);
+      var right = loadQueryData(right_query);
+      var merged_df = dfd.merge(left, right, on, how);
+      return merged_df
+  }
+
+
   
 };
 
