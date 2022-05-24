235ae6f1af96e4ba54b19e119da648197f4d762b
lzieglerpenn <lee.ziegler@penn-interactive.com>
2022-05-24 11:59:06 -0400

    updateTrace function testing and styles initial upload

--- pennviz.js
+++ pennviz.js
@@ -14,8 +14,34 @@ var pennviz = {
       return merged_df
   }
 
+  updateTrace: function(xval, yval, chartDiv, dfName, agg='sum') {
+    var selectedData = dfName.loc({rows:[xval], columns: [yval]});
+    var layout_update = {
+      title: 'some new title', // updates the title
+    };
+    // probs need to group by and shit
+    var groupedData = selectedData.groupby([xval]).sum();
+    var data_update = {
+		  'x': [groupedData[xval]],
+      'y': [groupedData[yval]]
+    };
 
-  
+	Plotly.update(chartDiv, data_update, layout_update);
+
+  }
+
+
+    
 };
+
+// add event listener for whichever element has xaxis class & yaxis class
+var xel = document.getElementsByClassName("xaxis")[0];
+var yel = document.getElementsByClassName("yaxis")[0];
+// need to add data-df and data-plot
+xel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, xel.dataset.plot, eval(xel.dataset.df)));
+yel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, yel.dataset.plot, eval(yel.dataset.df)));
+
+
+
 
 console.log('pennviz loaded');
