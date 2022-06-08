dfe4e935d664de7902b9473ed7be2c48d9de8c87
lzieglerpenn <lee.ziegler@penn-interactive.com>
2022-06-08 09:29:27 -0400

    update

--- pennviz.js
+++ pennviz.js
@@ -1,4 +1,4 @@
-// dependent on alamode & danfo libraries
+// dependent on alamode & danfo & Plotly libraries
 var pennviz = {
   
   loadQueryData: function(queryName){
@@ -8,13 +8,15 @@ var pennviz = {
   },
 
   mergeQuerys: function(left_query, right_query, on, how='inner'){
-      var left = loadQueryData(left_query);
-      var right = loadQueryData(right_query);
+      var holderl = alamode.getDataFromQuery(left_query);
+      var left = new dfd.DataFrame(holder1);
+      var holder2 = alamode.getDataFromQuery(right_query);
+      var right = new dfd.DataFrame(holder2);
       var merged_df = dfd.merge(left, right, on, how);
       return merged_df
   },
 
-  updateTrace: function(xval, yval, chartDiv, dfName, agg='sum') {
+  simpleExplore: function(xval, yval, chartDiv, dfName, agg='sum', plotType='bar') {
     var selectedData = dfName.loc({columns: [xval, yval]});
     var layout_update = {
       title: 'some new title', // updates the title
@@ -24,63 +26,69 @@ var pennviz = {
     var aggY = yval.concat('_', agg);
     var data_update = {
 		  'x': [groupedData[xval].values],
-      'y': [groupedData[aggY].values]
+      'y': [groupedData[aggY].values],
+      'type': plotType
     };
 
 	Plotly.update(chartDiv, data_update, layout_update);
 
-  },
-
-  openEars: function() {
-    // make X-Y axis mandatory in order for event listeners to work. Agg function should be optional
-    // must Open Ears in order to take input. Otherwise can just use other df stuff to make plots
-    // add event listener for whichever element has xaxis class & yaxis class & optionally agg class
-    try {
-      var xel = document.getElementsByClassName("xaxis")[0];
-    } catch(err) {
-      console.log(err);
-      var xel = False;
-    }
-    try {
-      var yel = document.getElementsByClassName("yaxis")[0];
-    } catch(err) {
-      console.log(err);
-      var yel = False;
-    }
-    try {
-      var aggel = document.getElementsByClassName("agg")[0];
-    } catch(err){
-      console.log(err);
-      var aggel = False;
-    }
-    
-    if (aggel) {
-      aggel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, yel.dataset.plot, eval(yel.dataset.df), agg=aggel.value));
-    } else {
-      var aggel = False;
-    }
-    // add event listener if xaxis class exists on an input
-    if (xel && aggel) {
-      xel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, xel.dataset.plot, eval(xel.dataset.df), agg=aggel.value));
-    } else if (xel) {
-      xel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, xel.dataset.plot, eval(xel.dataset.df)));
-    } else {
-      var xel = False;
-    }
-
-    // add event listener if yclass exists on an input
-    if (yel && aggel) {
-      yel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, yel.dataset.plot, eval(yel.dataset.df), agg=aggel.value));
-    } else if (yel) {
-      yel.addEventListener('change', pennviz.updateTrace(xel.value, yel.value, yel.dataset.plot, eval(yel.dataset.df)));
-    } else {
-      var yel = False;
-    }
   }
+
+  
 
     
 };
 
 
+function openEars(listen='simpleListen') {
+  // make X-Y axis mandatory in order for event listeners to work. Agg function should be optional
+  // must Open Ears in order to take input. Otherwise can just use other df stuff to make plots
+  // add event listener for whichever element has xaxis class & yaxis class & optionally agg class
+
+  // create function to run this one for simpleExplore
+  function simpleListen() {
+        // get possible listening elements
+        var xel = document.getElementsByClassName("xaxis")[0];
+        var yel = document.getElementsByClassName("yaxis")[0];
+        var aggel = document.getElementsByClassName("agg")[0];
+        var typel = document.getElementsByClassName("ptype")[0]; 
+
+        // create array with required & optional listeners
+        var els = [xel, yel];
+        if (aggel) {
+          els.push(aggel);
+          var aggval = aggel.value;
+        } else {
+          var aggval = 'sum';
+        }
+        if (typel) {
+          els.push(typel);
+          var typval = typel.value;
+        } else {
+          var typval = 'bar';
+        }
+
+        // add event listeners for all existences
+        for (item in els) {
+          if (item) {
+            item.addEventListener('change', 
+            pennviz.simpleExplore(
+              xel.value, 
+              yel.value, 
+              item.dataset.plot, 
+              eval(item.dataset.df), 
+              agg=aggval, 
+              plotType=typval)
+              );
+          } else {
+
+          };
+        }
+    // run function from openEars input
+    eval(listen);
+
+
+  };
+};
 
 console.log('pennviz loaded');
